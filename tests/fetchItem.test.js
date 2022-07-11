3fbb1b8decc18497152000cf4098f99ca94ed7d3
Pedro Nunes <pedro.icn88@gmail.com>
2022-07-11 19:43:48 -0300

    Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,26 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+
+  it('Verifica se fetch é chamada quando a função recebe o argumento "MLB1615760527"', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/items/MLB1615760527");
+  });
+
+  it('Teste se o retorno da função fetchItem com o argumento "MLB1615760527" é igual ao objeto item', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  
+  it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
+    expect(await fetchItem()).toEqual(new Error("You must provide an url"));
+  });
+
 });
