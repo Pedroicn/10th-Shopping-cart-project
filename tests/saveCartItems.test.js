069ad2bd417ee9105c747077b631c4f15c8fee50
Pedro Nunes <pedro.icn88@gmail.com>
2022-07-12 14:27:41 -0300

    Requisito 7

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -10,6 +10,6 @@ describe('3 - Teste a função saveCartItems', () => {
   });
   it('Verifica se ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
     saveCartItems('<ol><li>Item</li></ol>')
-    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
   });
 });
